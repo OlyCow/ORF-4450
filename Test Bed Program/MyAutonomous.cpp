a80610b63f7e474329ca3228e3c56c03e2a387c9
Ernest Gu <erniewarrensuper@gmail.com>
2013-02-17 08:45:24 -0800

    Added the program for the test bed.
    There shouldn't be much change to this in the near future...

---
+++ Test Bed Program/MyAutonomous.cpp
@@ -0,0 +1,38 @@
+#include "MyAutonomous.h"
+#include "LCD.h"
+
+MyAutonomous::MyAutonomous(MyRobot *MyRobot)
+{
+	LCD::ConsoleLog("MyAutonomous.constructor");
+	
+	myRobot = MyRobot;
+}
+	
+MyAutonomous::~MyAutonomous()
+{
+	LCD::ConsoleLog("MyAutonomous.destructor");
+}
+	
+void MyAutonomous::DoAutonomous(void)
+{
+	LCD::ConsoleLog("MyAutonomous.DoAutonomous");
+
+	LCD::PrintLine(4, "All: %d, Start=%d", myRobot->alliance, myRobot->startLocation);
+
+	myRobot->robotDrive.SetSafetyEnabled(false);
+	
+	//while (IsEnabled() && IsAutonomous())
+	//{
+		LCD::ConsoleLog("doing autonomous task %f", myRobot->ds->GetBatteryVoltage());
+
+		myRobot->robotDrive.Drive(0.5, 0.0); 	// drive forwards half speed
+	
+		Wait(2.0); 					// for 2 second
+		
+		LCD::ConsoleLog("out of autonomous wait");
+		
+		myRobot->robotDrive.Drive(0.0, 0.0); 	// stop robot
+	//}
+
+	LCD::ConsoleLog("MyAutonomous.DoAutonomous-end");
+}
