c3f0cd25bc3437ea3dd61e0453002b03a3643e0b
Ernest Gu <erniewarrensuper@gmail.com>
2013-03-01 14:42:19 -0800

    Started an outline for the DriveBase class

--- ORFv2/Systems/DriveBase.h
+++ ORFv2/Systems/DriveBase.h
@@ -11,6 +11,27 @@ public:
 	DriveBase(Robot *robot);
 	~DriveBase();
 	
+	void drive();
+
+	void polarSetDriveBase(	float angle,
+							float magnitude,
+							float rotation,
+							float heading	);
+	void rectangularSetDriveBase(	float x,
+									float y,
+									float rotation,
+									float heading	);
+
+	void setFrontLMotor(float power);
+	void setFrontRMotor(float power);
+	void setRearLMotor(float power);
+	void setRearRMotor(float power);
+
+	float getFrontLMotor();
+	float getFrontRMotor();
+	float getRearLMotor();
+	float getRearRMotor();
+
 	
 private:
 	Robot *robot;
